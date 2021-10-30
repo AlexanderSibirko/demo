7887964a8c73d8a48fbe580a774e27e15ae52b3e
AlexanderSibirko <89727680+AlexanderSibirko@users.noreply.github.com>
2021-10-30 11:44:28 +0300

    Added diff multiplier counter

--- repo/Program.cs
+++ repo/Program.cs
@@ -62,9 +62,41 @@ int MinCounter(int n)
     return simplenumber[index];
 }
 
+int DiffCounter(int n)
+{
+    int [] simplenumber= {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199};
+    int count = 0;
+    bool newcount = true;
+    int index = 0;
+    while (n > 1)
+    {
+            if (n % simplenumber[index] == 0)
+            {
+                n = n / simplenumber[index];
+                //Console.WriteLine(simplenumber[index]);
+                if (newcount)
+                {
+                    count = count + 1;
+                    newcount = false;
+                    //Console.WriteLine(simplenumber[index]);
+                }
+            }
+            else
+            {
+                newcount = true;
+                index++;
+            }
+    }
+    return count;
+}
+
+
 //int n = 32; // 1..2_147_000_000
 int result = Counter(24);
 Console.WriteLine($"result = {result}");
 
 result = MinCounter(41);
 Console.WriteLine($"Минимальный делитель равен: {result}");
+
+result = DiffCounter(637); //7*7*13
+Console.WriteLine($"Количество различных множителей составляет: {result}");
