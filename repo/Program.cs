be389fb28922ffa3a8a4c4c30eeea3db9ca8b30b
AlexanderSibirko <89727680+AlexanderSibirko@users.noreply.github.com>
2021-10-30 11:28:20 +0300

    Update Program.cs

--- repo/Program.cs
+++ repo/Program.cs
@@ -44,6 +44,27 @@ int Counter(int n)
     return count;
 }
 
+int MinCounter(int n)
+{
+    int [] simplenumber= {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199};
+    int index = 0;
+    while (n > 1)
+    {
+        if (n % simplenumber[index] == 0)
+        {
+            break;
+        }
+        else
+        {
+            index++;
+        }
+    }
+    return simplenumber[index];
+}
+
 //int n = 32; // 1..2_147_000_000
 int result = Counter(24);
 Console.WriteLine($"result = {result}");
+
+result = MinCounter(41);
+Console.WriteLine($"Минимальный делитель равен: {result}");
